add7a6751aaafc713b41a027f870cb50efd4975e
Artnod <artnod78@users.noreply.github.com>
2017-10-30 17:45:21 +0100

    Add files via upload

--- logClient/js/apiKill.js
+++ logClient/js/apiKill.js
@@ -1,84 +1,98 @@
-var nbLine = 0;
-var initNbLine = 0;
+var nbLine = null;
+var player = null;
 var nbdiv = 0;
-function StartKillModule () {
-    var maxLinesPerRequest = 50;
-    var timeout = null;
-    var body = $("body");
-    var lastRead = -1;
 
-    var updateEvent = function() {
-        // on se place à la fin des log
-        if(nbLine == 0){
-			$.getJSON( "api/getlog.php?firstLine=" + (initNbLine)).done(function(data){
-				if (data.entries.length > 0) {
-					initNbLine = data.lastLine;
-                }
-				if((data.firstLine) == data.lastLine){
-					nbLine = initNbLine;
-					console.log("End of Log");
-					
-				}
-				else{
-					console.log("Log Init");
-				}
-				
-			})
-			.fail(function(jqxhr, textStatus, error) {
-				nbLine = 0;
-                console.log("error init");
-			});
-			timeout = window.setTimeout(updateEvent, 500);
+var firstRun = true;
+var timeout = null;
+// GET DATA
+function getLog(){
+	var mydata = [];
+	$.ajax({
+		url: 'api/getlog.php?firstLine=' + nbLine,
+		async: false,
+		dataType: 'json',
+		success: function (jsonData) {
+			mydata = jsonData;
 		}
-        else{
-            $.getJSON("api/getlog.php?firstLine=" + (nbLine))
-            .done(function(data) {
-				if(data.firstLine == data.lastLine){
-					if(data.maxLog == data.firstLine){
-						console.log("No new log");
-					}
-					else{
-						nbLine = 0;
-						initNbLine = 0;
-						console.log("New log File");
-					}
-				}
-				else{
-                    console.log("New log Read");
-					// Pour chaque nouvelle ligne de log
-					for (var i = 0; i < data.entries.length; i++) {
-						var logMsg = data.entries[i].msg;
-						var tmpLogMsg = logMsg.match("GMSG: Player \'(.*)\' died");
-						// si un joueur est mort
-						if(tmpLogMsg){
-							// on recupere le nom du joueur
-							var playerName = tmpLogMsg[1];
-							// on ajoute du contenu
-							$(".div"+(nbdiv-1)).remove();
-							$("body").append($("<div></div>").attr("id", "killContainer").addClass("div" + nbdiv));
-							$("#killContainer").append($("<div></div>").attr("id", "killHeader"));
-							$("#killHeader").append($("<span>Alerte Troll</span>").attr("id", "spanHeader"));
-							$("#killContainer").append($("<div></div>").attr("id", "killContent"));
-							$("#killContent").append($("<image id='sdtdLogo' src='img/Brokenbone_icon.png'>"));
-							$("#killContent").append($("<div></div>").attr("id", "killContentText"));
-							$("#killContentText").append($("<span>" +  playerName + " est mort! Il aurait dû payer son atelle !!!" + "</span>"));
-							nbdiv = nbdiv + 1;
-							console.log(playerName+" died");
-						}
-					}
-					// on met a jour le numero de la derniere ligne lu
-					nbLine = data.lastLine;
-				}
-            })
-            .fail(function(jqxhr, textStatus, error) {
-                    console.log("error read log");
-            })
-            .always(function() {
-            });
-            timeout = window.setTimeout(updateEvent, 1000);
-
-                }
-        };
-        updateEvent ();
+	});
+	return mydata;
 }
-StartKillModule();
+
+function updateOverlay(){
+	// on ajoute du contenu
+	var body = $("body");
+	$(".div"+(nbdiv-1)).remove();
+	$("body").append($("<div></div>").attr("id", "killContainer").addClass("div" + nbdiv));
+	$("#killContainer").append($("<div></div>").attr("id", "killHeader"));
+	$("#killHeader").append($("<span>Alerte Troll</span>").attr("id", "spanHeader"));
+	$("#killContainer").append($("<div></div>").attr("id", "killContent"));
+	$("#killContent").append($("<image id='sdtdLogo' src='img/Brokenbone_icon.png'>"));
+	$("#killContent").append($("<div></div>").attr("id", "killContentText"));
+	$("#killContentText").append($("<span>" +  player + " est mort<br>comme un fragile !!!" + "</span>"));
+	nbdiv = nbdiv + 1;
+}
+
+// MAIN FUNCTION
+function getLastLine(){
+	var mydata = [];
+	$.ajax({
+		url: 'api/getlog.php',
+		async: false,
+		dataType: 'json',
+		success: function (jsonData) {
+			mydata = jsonData.maxLog;
+		}
+	});
+	return mydata;
+}
+function getFirstRun(){
+	// initialisation au premier lancement
+	if(firstRun == true){		
+		// on recupere le numero de la dernière ligne
+		nbLine = getLastLine();
+		console.log("First Run\tLast Line\t" + nbLine);
+		firstRun = false;
+	}
+}
+
+function scanLog(){
+	var logs = getLog();
+	if (logs.entries.length == 0){
+		if(logs.maxLog < logs.lastLine){// New log file
+			console.log("Get Log\t\tSuccess\t\tNew log File");
+			firstRun = true;
+		}
+		else {// No new log
+			console.log("Get Log\t\tSuccess\t\tNo new log\t\t"+nbLine);
+		}
+		
+	}
+	else{// New log		
+		console.log("Get Log\t\tSuccess\t\tNew log\t\t\t" + nbLine);		
+		// Pour chaque nouvelle ligne de log
+		for (var i = 0; i < logs.entries.length; i++) {
+			var logLine = logs.entries[i].msg;
+			// si un joueur est mort
+			var playerDiedMsg = logLine.match("GMSG: Player \'(.*)\' died");
+			if(playerDiedMsg){
+				// on recupere le nom du joueur
+				player = playerDiedMsg[1];
+				// on met à jour l'overlay
+				updateOverlay();
+				console.log("Log\t\t\tPlayer\t\tDied\t\t\t"+player);
+			}
+		}
+	}
+	// on met a jour le numero de la derniere ligne lu
+	nbLine = logs.maxLog;
+}
+
+
+
+var updateEvent = function() {
+	getFirstRun();
+	scanLog();
+	timeout = window.setTimeout(updateEvent, 2000);
+};
+updateEvent();
+
